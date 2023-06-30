60c29e89f2b29fa44ca1047779b5bb1b44eedfa1
Germán Fera <feragerman@gmail.com>
2023-06-30 10:17:51 -0300

    Deploy to GH pages updated

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:gfera/dme-map-finder.git master:gh-pages
+git push -f git@github.com:gfera/dme-map-finder.git main:gh-pages
 
 cd -
